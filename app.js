029c3c86b6824f2555aadb0c5ae8e7adbc88734f
Suba Radhakrishnan <Suba Radhakrishnan>
2016-08-13 02:33:20 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿var restify = require('restify');
+var builder = require('botbuilder');
+
+// Bot setup
+
+// Setup restify server
+var server = restify.createServer();
+server.listen(process.port || process.env.PORT || 3978, function () {
+    console.log('%SListen to %s', server.name, server.url, server);
+});
+
+//Create Chatbot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//Bot Dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
